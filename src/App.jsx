957fbae5e57bedf9bf26e6e44d6e9f4b8a6c2e2d
caprarim <aaaassdddcgvv@gmail.com>
2026-02-14 13:48:00 +0500

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,22 @@ import ToDoList from './homePage.jsx'
 import ToDoLi from './functionToDoList.jsx'
 import Register from './signUp.jsx'
 
-import{
-  BrowserRouter,
+import {
+  HashRouter as Router,
   Routes,
   Route,
 } from 'react-router-dom';
 
 function App() {
-
-
   return (
-    
-    <>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<ToDoLi></ToDoLi>}></Route>
-    <Route path='/sendHome' element={<ToDoList />}/>
-    <Route path='/todo' element={<ToDoLi />}/>
-    <Route path='/sign' element={<Register />}></Route>
-    </Routes>
-    </BrowserRouter>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<ToDoLi />} />
+        <Route path='/sendHome' element={<ToDoList />} />
+        <Route path='/todo' element={<ToDoLi />} />
+        <Route path='/sign' element={<Register />} />
+      </Routes>
+    </Router>
   )
 }
 
